71633649caf36261e4791dbc2104cc5a0d48760f
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-04-21 22:07:31 -0400

    Run backend through docker-compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,10 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-
-// Going to / will simply load the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('hello world - edited');
 });
 
 app.get('/:shortUrl', (req, res) => {
@@ -20,6 +17,6 @@ app.post('/api/short-url', (req, res) => {
   res.send(req.body.url);
 });
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
+app.listen(8000, () => {
+  console.log('Listening on port 8000 - edited');
 });
