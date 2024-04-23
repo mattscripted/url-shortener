601fc171e27e378c85274392940237a0b379754e
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-04-23 11:43:21 -0400

    Create /short-url-api with stub endpoints

---
+++ short-url-api/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// TODO: Use database
+const shortUrls = {
+  'google': 'http://www.google.com/'
+}
+
+app.get('/api/short-url/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  console.log(shortUrl);
+
+  if (shortUrls[shortUrl]) {
+    return res.send(shortUrls[shortUrl])
+  }
+
+  res.status(404).send('Invalid short URL');
+});
+
+app.post('/api/short-url', (req, res) => {
+  const url = req.body.url;
+  const shortUrl = 'test';
+  shortUrls[shortUrl] = req.body.url;
+
+  res.send(shortUrl);
+});
+
+app.listen(8001, () => {
+  console.log('Listening on port 8001');
+});
