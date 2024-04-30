be47d0bf5070a74d5f261eeff6afc50ff4bfea11
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-04-30 16:30:24 -0400

    Create short URL from form upon submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,64 @@
-import { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import axios from 'axios';
 
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  useEffect(() => {
-    const createShortUrl = async () => {
-      const url = 'http://mattshelley.dev/';
-  
-      try {
-        const response = await axios.post('http://localhost:8000/api/short-url', {
-          url,
-        });
-  
-        console.log(response);
-      } catch (error) {
-        console.log(error)
-      }
-    }
+  const initialValues = {
+    url: '',
+  };
+  const validationSchema = Yup.object().shape({
+    url: Yup.string().url('Invalid URL').required('Required'),
+  });
 
-    createShortUrl();
-  }, []);
+  const handleSubmit = async (values) => {
+    console.log('submit form', values);
+    const { url } = values;
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/short-url', {
+        url,
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ handleSubmit, handleChange, values, touched, errors }) => (
+        <Container>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Enter a URL to shorten:</Form.Label>
+              <Form.Control
+                type="text"
+                name="url"
+                placeholder="http://www.google.com/"
+                value={values.url}
+                onChange={handleChange}
+                isValid={touched.url && !errors.url}
+              />
+              <Form.Control.Feedback>
+                {errors.url}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Shorten
+            </Button>
+          </Form>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
