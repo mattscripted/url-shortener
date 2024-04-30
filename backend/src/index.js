e86678fac0d97a07870eccd82663839509f26d0e
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-04-30 14:28:27 -0400

    Stub our backend API endpoints for redirect and creating short-url

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,14 @@ app.get('/status', (req, res) => {
   res.send('ok');
 });
 
+app.get('/:shortUrlHash', (req, res) => {
+  res.redirect('http://www.google.com/');
+});
+
+app.post('/api/short-url', (req, res) => {
+  res.send('create a short-url');
+});
+
 app.listen(8000, () => {
   console.log('Listening on port 8000');
 });
