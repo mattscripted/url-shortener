f179a51fc3b40f20b5c1174ec4537d2752d3c9c5
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-04-30 14:46:26 -0400

    Simulate creating and getting short URL with mock db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,64 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { nanoid } = require('nanoid');
 
 const app = express();
+app.use(bodyParser.json());
+
+const SHORT_URLS = {};
 
 app.get('/status', (req, res) => {
   res.send('ok');
 });
 
 app.get('/:shortUrlHash', (req, res) => {
-  res.redirect('http://www.google.com/');
+  const shortUrlHash = req.params.shortUrlHash;
+  const url = SHORT_URLS[shortUrlHash];
+
+  if (url) {
+    res.redirect('http://www.google.com/');
+  } else {
+    res.status(404).json({
+      errors: [{
+        status: 404,
+        detail: 'No URL found for provided short URL hash.'
+      }]
+    });
+  }
 });
 
 app.post('/api/short-url', (req, res) => {
-  res.send('create a short-url');
+  try {
+    const url = req.body.url;
+    
+    // Generate a random, mostly unique hash
+    // Note: There could be collisions, so there is probably a better hashing method
+    const shortUrlHash = nanoid();
+
+    SHORT_URLS[shortUrlHash] = url;
+
+    // const shortUrl = new ShortUrl({ shortUrlHash, url });
+    // await shortUrl.save();
+
+    res.json({
+      data: {
+        type: 'shortUrl',
+        id: shortUrlHash,
+        attributes: {
+          url,
+          // url: shortUrl.url
+        }
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      errors: [{
+        status: 500,
+        detail: 'Unable to create short URL.'
+      }]
+    });
+  }
 });
 
 app.listen(8000, () => {
