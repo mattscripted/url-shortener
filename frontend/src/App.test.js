83a4c2522cf38e7f0eed63353552a8f3c325a599
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-05-05 12:21:05 -0400

    Test invalid and value URLs on for submission

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,34 +1,88 @@
-import { render, screen } from '@testing-library/react';
+import { render as rtlRender, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import App from './App';
+
+jest.mock('axios');
+
+function render(ui) {
+  const user = userEvent.setup();
+  rtlRender(ui);
+  
+  const getUrlTextbox = () => screen.getByRole('textbox', {
+    name: 'Enter a URL to shorten:',
+  });
+  const getSubmitButton = () => screen.getByRole('button', { name: 'Shorten' });
+
+  return {
+    user,
+    getUrlTextbox,
+    getSubmitButton,
+  }
+}
 
 describe('App', () => {
   it('renders a text input to enter a short URL', () => {
-    render(<App />);
-    expect(screen.getByRole('textbox', {
-      name: 'Enter a URL to shorten:',
-    })).toBeInTheDocument();
+    const { getUrlTextbox } = render(<App />);
+    expect(getUrlTextbox()).toBeInTheDocument();
   });
 
   it('renders a "Shorten" button', () => {
-    render(<App />);
-    expect(screen.getByRole('button', { name: 'Shorten' })).toBeInTheDocument();
+    const { getSubmitButton } = render(<App />);
+    expect(getSubmitButton()).toBeInTheDocument();
   });
 
   it('renders a "Required" error, if the URL is empty', async () => {
-    const user = userEvent.setup();
-    render(<App />);
+    const { user, getUrlTextbox, getSubmitButton } = render(<App />);
+    const urlTextbox = getUrlTextbox();
+    const submitButton = getSubmitButton();
+    const errorMessage = 'Required';
 
-    const urlTextbox = screen.getByRole('textbox', {
-      name: 'Enter a URL to shorten:',
-    });
-    const submitButton = screen.getByRole('button', { name: 'Shorten' })
-
-    expect(screen.queryByText('Required')).not.toBeInTheDocument();
+    expect(screen.queryByText(errorMessage)).not.toBeInTheDocument();
 
     await user.click(urlTextbox);
     await user.click(submitButton);
 
-    expect(await screen.findByText('Required')).toBeInTheDocument();
+    expect(await screen.findByText(errorMessage)).toBeInTheDocument();
+  });
+
+  it('renders an "Invalid URL" error, if the URL is invalid', async () => {
+    const { user, getUrlTextbox, getSubmitButton } = render(<App />);
+    const urlTextbox = getUrlTextbox();
+    const submitButton = getSubmitButton();
+    const errorMessage = 'Invalid URL';
+
+    expect(screen.queryByText(errorMessage)).not.toBeInTheDocument();
+
+    await user.type(urlTextbox, 'not-a-url');
+    await user.click(submitButton);
+
+    expect(await screen.findByText(errorMessage)).toBeInTheDocument();
+  });
+
+  it('submits the form, and renders success state', async () => {
+    const url = 'http://www.google.com/';
+    axios.post.mockResolvedValueOnce({
+      data: {
+        data: {
+          type: 'shortUrl',
+          id: 'short-url-hash',
+          attributes: {
+            url,
+          }
+        },
+      }
+    });
+
+    const { user, getUrlTextbox, getSubmitButton } = render(<App />);
+    const urlTextbox = getUrlTextbox();
+    const submitButton = getSubmitButton();
+
+    await user.type(urlTextbox, url);
+    await user.click(submitButton);
+
+    // expect(axios.post).toHaveBeenCalledWith('/api/short-url')
+
+    expect(await screen.findByText('Your short URL can be accessed here:')).toBeInTheDocument();
   });
 });
