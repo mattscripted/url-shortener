fefe47ca26742d663fe6c3663cb74e7e05d1f205
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-05-07 21:44:53 -0400

    Move routes to their own file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,77 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-const { nanoid } = require('nanoid');
-const ShortUrl = require('./models/ShortUrl');
+const router = require('./routes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get('/status', (req, res) => {
-  res.send('ok');
-});
-
-app.get('/:shortUrlHash', async (req, res) => {
-  const shortUrlHash = req.params.shortUrlHash;
-  const shortUrl = await ShortUrl.findOne({ shortUrlHash })
-
-  if (shortUrl) {
-    res.redirect(shortUrl.url);
-  } else {
-    res.status(404).json({
-      errors: [{
-        status: 404,
-        detail: 'No URL found for provided short URL hash.'
-      }]
-    });
-  }
-});
-
-app.post('/api/short-url',
-  body('url').isURL(),
-  async (req, res) => {
-    const result = validationResult(req);
-
-    if (!result.isEmpty()) {
-      return res.status(400).json({
-        errors: [{
-          status: 400,
-          detail: 'Invalid URL provided.',
-        }],
-      });
-    }
-
-    try {
-      const url = req.body.url;
-      
-      // Generate a random, mostly unique hash
-      // Note: There could be collisions, so there is probably a better hashing method
-      const shortUrlHash = nanoid();
-      const shortUrl = new ShortUrl({ shortUrlHash, url });
-      await shortUrl.save();
-
-      res.json({
-        data: {
-          type: 'shortUrl',
-          id: shortUrlHash,
-          attributes: {
-            url: shortUrl.url
-          }
-        },
-      });
-    } catch (error) {
-      res.status(500).json({
-        errors: [{
-          status: 500,
-          detail: 'Unable to create short URL.',
-        }],
-      });
-    }
-  }
-);
+app.use('/', router);
 
 async function main() {
   try {
