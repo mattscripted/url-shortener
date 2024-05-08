bb51a70331d83c222cda99d415891a8144ee7df3
mattscripted <46871802+mattscripted@users.noreply.github.com>
2024-05-08 10:23:00 -0400

    Install and connect to Redis cache

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const redis = require('redis');
 const router = require('./routes');
 
 const app = express();
@@ -9,18 +10,34 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', router);
 
+;
+
 async function main() {
   try {
     const mongodbUrl = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@database:27017`;
     await mongoose.connect(mongodbUrl);
-    console.log('Connected to MongoDB');
-
-    app.listen(8000, () => {
-      console.log('Listening on port 8000');
-    });
+    console.log('Connected to MongoDB');    
   } catch (error) {
-    console.log('Unable to connect to MongoDB', error)
+    console.log('Unable to connect to MongoDB', error);
+    return;
   }
+
+  try {
+    const redisUrl = 'redis://cache';
+    const redisClient = await redis.createClient({
+      url: redisUrl
+    });
+    await redisClient.connect();
+
+    console.log('Connected to Redis');
+  } catch (error) {
+    console.log('Unable to connect to Redis', error);
+    return;
+  }
+
+  app.listen(8000, () => {
+    console.log('Listening on port 8000');
+  });
 }
 
 main();
